5932fde2f49ebe57d63f1c95b888311a6fd9410a
praneeth <praneeth@mit.edu>
2025-03-11 10:58:52 -0400

    refactor

--- docs/api.md
+++ docs/api.md
@@ -3,5 +3,10 @@
 ```{eval-rst}
 .. automodule:: pysampled
     :members:
+    :special-members:
+
+.. automodule:: pysampled.core
+    :members:
+    :undoc-members:
     :special-members: __getitem__, __call__
 ```
--- docs/conf.py
+++ docs/conf.py
@@ -6,10 +6,17 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+sys.path.insert(0, os.path.abspath("../pysampled"))
+
+from pysampled.__version__ import __version__
+
 project = "pysampled"
 copyright = "2025, Praneeth Namburi"
 author = "Praneeth Namburi"
-release = "1.0.0"
+version = __version__
+release = __version__
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
